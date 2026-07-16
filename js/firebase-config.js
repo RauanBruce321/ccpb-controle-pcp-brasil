4110c8b95d086189f75c1ba971faf097f77198b3
RauanBruce321 <bruce852rauan@outlook.com.br>
2026-07-16 11:44:43 -0300

    Projeto organizado CCPB

---
+++ js/firebase-config.js
@@ -0,0 +1,11 @@
+// Configuração do projeto Firebase CCPB.
+// Este arquivo está preparado para a etapa de sincronização online.
+window.CCPB_FIREBASE_CONFIG = {
+  apiKey: "AIzaSyBjNhnkVw2SBKGdDagELzv1EOF2cw_o3tw",
+  authDomain: "ccpb-controle-pcp-brasil.firebaseapp.com",
+  databaseURL: "https://ccpb-controle-pcp-brasil-default-rtdb.firebaseio.com",
+  projectId: "ccpb-controle-pcp-brasil",
+  storageBucket: "ccpb-controle-pcp-brasil.firebasestorage.app",
+  messagingSenderId: "770466864881",
+  appId: "1:770466864881:web:847b45d5398578fd159350"
+};
